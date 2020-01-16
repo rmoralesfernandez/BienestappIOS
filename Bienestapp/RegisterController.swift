31eb1ba8e946efa19fd4df3660c0d489d18f28ae
Ricardo Morales <ricardomoralesfer@yahoo.com>
2020-01-16 09:47:42 +0100

    Initial Commit

---
+++ Bienestapp/RegisterController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterController.swift
+//  Bienestapp
+//
+//  Created by alumnos on 16/01/2020.
+//  Copyright © 2020 alumnos. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
