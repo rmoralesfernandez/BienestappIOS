e036390fd376555d9122ac0438675601caabc4d2
Ricardo Morales <ricardomoralesfer@yahoo.com>
2020-01-20 09:46:53 +0100

    recover password working

--- Bienestapp/LoginController.swift
+++ Bienestapp/LoginController.swift
@@ -19,11 +19,14 @@ class LoginController: UIViewController {
     }
     
     @IBAction func login(_ sender: Any) {
-        if(emailTF.text! == user.email && passwordTF.text! == user.password){
+        if(emailTF.text!.isEmpty && passwordTF.text!.isEmpty){
             print("siuuuu")
-            postUser(user: user)
+            
         }else{
             print("buuuuu")
+            user.email = emailTF.text!
+            user.password = passwordTF.text!
+            postUser(user: user)
         }
     }
     
@@ -31,7 +34,7 @@ class LoginController: UIViewController {
         let url = URL(string: "http://localhost:8888/Rick/BienestappRick/public/index.php/api/login")
         let json = ["email": user.email,
                     "password": user.password]
-        
+        print(json)
         Alamofire.request(url!, method: .post, parameters: json, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
             print(response)
         }
--- Bienestapp/RecoverController.swift
+++ Bienestapp/RecoverController.swift
@@ -5,7 +5,7 @@
 //  Created by alumnos on 16/01/2020.
 //  Copyright © 2020 alumnos. All rights reserved.
 //
-
+import Alamofire
 import UIKit
 
 class RecoverController: UIViewController {
@@ -16,12 +16,24 @@ class RecoverController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
     
     @IBAction func recoverPassword(_ sender: Any) {
-        if (emailTF.text! == user.email) {
+        if (!emailTF.text!.isEmpty) {
             print("todo correcto")
+            user.email = emailTF.text!
+            postUser(user: user)
         }else{
-            print("este email no coincide con uno existente")
+            print("EL campo no puede estar vacio")
+        }
+    }
+    
+    func postUser(user: User) {
+        let url = URL(string: "http://localhost:8888/Rick/BienestappRick/public/index.php/api/recover_password")
+        let json = ["email": user.email]
+        print(json)
+        Alamofire.request(url!, method: .post, parameters: json, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
+            print(response)
         }
     }
     
