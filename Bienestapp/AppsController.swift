d04a0ad793eb15b445eb6cc5a1ac971544665ffa
Ricardo Morales <ricardomoralesfer@yahoo.com>
2020-01-27 13:18:48 +0100

    save the token in the login

--- Bienestapp/AppsController.swift
+++ Bienestapp/AppsController.swift
@@ -15,4 +15,29 @@ class AppsController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 }
+    
+    func getUser(user: User) {
+        let url = URL(string: "http://localhost:8888/Rick/BienestappRick/public/index.php/api/")
+        let json = ["email": user.email,
+                    "password": user.password]
+        print(json)
+        Alamofire.request(url!, method: .post, parameters: json, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
+            print(response)
+            if response.response!.statusCode == 201 {
+                self.performSegue(withIdentifier: "CorrectLogin", sender: nil)
+                var json = response.result.value as! [String: AnyObject]
+                print(json)
+                token = json["token"] as! String
+                print(token)
+            } else {
+                let alert1 = UIAlertAction(title:"Cerrar", style: UIAlertAction.Style.default) {
+                    (error) in
+                }
+                let alert = UIAlertController(title: "Error", message:
+                    "Informacion Incorrecta", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(alert1)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
 }
---
+++ Bienestapp/tableCell.swift
@@ -0,0 +1,15 @@
+//
+//  tableCell.swift
+//  Bienestapp
+//
+//  Created by alumnos on 27/01/2020.
+//  Copyright © 2020 alumnos. All rights reserved.
+//
+
+import UIKit
+
+class tableCell: UITableViewCell {
+    @IBOutlet weak var imageApp: UIImageView!
+    @IBOutlet weak var NameText: UILabel!
+    @IBOutlet weak var TimeText: UILabel!
+}
